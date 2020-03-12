a0f6434fcb40793037158ada3982462118b354a4
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2020-03-12 12:05:34 +0000

    [entities]added video entity

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Educast.Models;
+
+namespace Educast.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly VideoContext _context;
+
+        public VideoController(VideoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Video
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Video>>> GetVideo()
+        {
+            return await _context.Video.ToListAsync();
+        }
+
+        // GET: api/Video/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Video>> GetVideo(Guid id)
+        {
+            var video = await _context.Video.FindAsync(id);
+
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return video;
+        }
+
+        // PUT: api/Video/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVideo(Guid id, Video video)
+        {
+            if (id != video.uuid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(video).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VideoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Video
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Video>> PostVideo(Video video)
+        {
+            _context.Video.Add(video);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVideo", new { id = video.uuid }, video);
+        }
+
+        // DELETE: api/Video/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Video>> DeleteVideo(Guid id)
+        {
+            var video = await _context.Video.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            _context.Video.Remove(video);
+            await _context.SaveChangesAsync();
+
+            return video;
+        }
+
+        private bool VideoExists(Guid id)
+        {
+            return _context.Video.Any(e => e.uuid == id);
+        }
+    }
+}
